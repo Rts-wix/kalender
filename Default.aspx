8f9b676a472b4dc3114951f007580c790927a24a
sm <sm@rts.dk>
2013-05-08 14:01:04 +0200

    Startup på kalender

---
+++ Default.aspx
@@ -0,0 +1,79 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        div.day
+        {
+            border: 1px solid blue;
+            margin-bottom: 1px;
+            display:inline-block;
+            float:left;
+            width:12%;
+        }
+        
+        /* mandag */
+        div.day:nth-child(7n+6)
+        {
+            clear:both;
+            background-color:Aqua;
+        }
+        
+        div.calendar
+        {
+            clear:both;
+            margin-bottom:20px;
+            background-color:Silver;
+            float:left;
+            width:90%;
+        }
+        
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <div class="calendar">
+        <asp:Repeater ID="RepeaterMonth1" runat="server" 
+            onitemdatabound="RepeaterMonth1_ItemDataBound">
+            <ItemTemplate>
+                <div class="day">
+                    <p><%# Eval("day") %></p>
+                    <asp:Repeater ID="RepeaterDay1" runat="server">
+                       <ItemTemplate>
+                            <div>
+                                <%# Eval("start_time") %>
+                                <%# Eval("title") %>
+                            </div>
+                       </ItemTemplate>
+                    </asp:Repeater>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>
+   
+    <div class="calendar">
+        <asp:Repeater ID="RepeaterMonth2" runat="server" 
+            onitemdatabound="RepeaterMonth2_ItemDataBound">
+            <ItemTemplate>
+                <div class="day">
+                    <p><%# Eval("day") %></p>
+                    <asp:Repeater ID="RepeaterDay2" runat="server">
+                       <ItemTemplate>
+                            <div>
+                                <%# Eval("start_time") %>
+                                <%# Eval("title") %>
+                            </div>
+                       </ItemTemplate>
+                    </asp:Repeater>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
+    </div>    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+public partial class _Default : System.Web.UI.Page
+{
+    private List<CalEvent> eventsThisMonth;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // Repeater 1
+        List<DateTime> month = new List<DateTime>();
+        for(DateTime d = DateTime.Now.AddDays( - DateTime.Now.Day +1 ); 
+            d.Month == DateTime.Now.Month ; 
+            d=d.AddDays(1) )
+        {
+            month.Add(d);
+        }
+        RepeaterMonth1.DataSource = month;
+        RepeaterMonth1.DataBind();
+
+        // Andet forsøg
+        // Repeater 2
+        eventsThisMonth = new List<CalEvent>();
+        using (calendarContext ctx = new calendarContext())
+        {
+            DateTime StartMonth = ((DateTime)DateTime.Now.AddDays(-DateTime.Now.Day));
+            DateTime EndMonth = ((DateTime)DateTime.Now.AddDays(-DateTime.Now.Day).AddMonths(1));
+            var events2 = ctx.CalEvents
+                .Where(x => x.start_date > StartMonth
+                            && x.start_date <= EndMonth);
+            foreach (var cEvent in events2)
+            {
+                eventsThisMonth.Add(cEvent);
+            }
+            RepeaterMonth2.DataSource = month;
+            RepeaterMonth2.DataBind();
+        }
+
+    }
+
+    protected void RepeaterMonth1_ItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+        using (calendarContext ctx = new calendarContext())
+        {
+            var events = ctx.CalEvents
+                        .Where(x => x.start_date == ((DateTime)e.Item.DataItem).Date)
+                        .Select(ev => ev);
+
+            Repeater RepeaterDay1 = ((Repeater)e.Item.FindControl("RepeaterDay1"));
+
+            if (RepeaterDay1 != null)
+            {
+                RepeaterDay1.DataSource = events;
+                RepeaterDay1.DataBind();
+            }
+        }
+    }
+
+    protected void RepeaterMonth2_ItemDataBound(object sender, RepeaterItemEventArgs e)
+    {
+        using (calendarContext ctx = new calendarContext())
+        {
+            var events = this.eventsThisMonth
+                        .Where(x => x.start_date == ((DateTime)e.Item.DataItem).Date)
+                        .Select(ev => ev);
+
+            Repeater RepeaterDay2 = ((Repeater)e.Item.FindControl("RepeaterDay2"));
+
+            if (RepeaterDay2 != null)
+            {
+                RepeaterDay2.DataSource = events;
+                RepeaterDay2.DataBind();
+            }
+        }
+    }
+
+}
